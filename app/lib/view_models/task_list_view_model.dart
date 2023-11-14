ebdc5d6e38ec577ed111d5024d80d342710b652a
Hannah Harhai <hannahharhai@icloud.com>
2023-11-14 15:52:29 -0500

    bb

---
+++ app/lib/view_models/task_list_view_model.dart
@@ -0,0 +1,34 @@
+import 'package:app/view_models/task_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:app/models/task.dart';
+
+
+class TaskListViewModel extends ChangeNotifier {
+
+  List<TaskViewModel> _tasks = <TaskViewModel>[];
+
+  List<TaskViewModel> get tasks => _tasks;
+  
+  void addTask(TaskViewModel task) {
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void deleteTask(int index) {
+    if (index >= 0 && index < _tasks.length) {
+      _tasks.removeAt(index);
+      notifyListeners();
+    }
+  }
+
+  void editTask(int index, Task updatedTask) {
+  if (index >= 0 && index < _tasks.length) {
+    _tasks[index] = updatedTask;
+    notifyListeners();
+  }
+}
+
+
+
+
+}
