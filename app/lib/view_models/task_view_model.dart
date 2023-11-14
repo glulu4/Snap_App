ea827aa6069d42e2b9f0b829bd0c558c67c733c7
Gabriel Lulu <glulu4444@gmail.com>
2023-11-14 15:54:09 -0500

    sxdfghjk

--- app/lib/view_models/task_view_model.dart
+++ app/lib/view_models/task_view_model.dart
@@ -2,28 +2,16 @@ import 'package:app/models/task.dart';
 
 class TaskViewModel {
 
-<<<<<<< Updated upstream
-=======
-class TaskViewModel {
->>>>>>> Stashed changes
   final Task task;
 
   TaskViewModel({required this.task});
 
   String get title {
-<<<<<<< Updated upstream
-    return this.task.title;
-  }
-
-  DateTime get dueDate {
-    return this.task.dueDate;
-=======
     return task.title;
   }
 
   String get poster {
     return task.posterUrl;
->>>>>>> Stashed changes
   }
 
   String get category {
